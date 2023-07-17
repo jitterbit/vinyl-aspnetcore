d39eb40f123de3aebad705180dcf2e12ef1a3d39
Mackinnon Buck <mackinnon.buck@gmail.com>
2023-07-16 23:13:02 -0700

    Rough prototype

--- src/Components/Web.JS/src/Boot.Web.ts
+++ src/Components/Web.JS/src/Boot.Web.ts
@@ -10,24 +10,22 @@
 
 import { DotNet } from '@microsoft/dotnet-js-interop';
 import { startCircuit } from './Boot.Server.Common';
-import { startWebAssembly } from './Boot.WebAssembly.Common';
+import { loadWebAssemblyPlatform, startWebAssembly } from './Boot.WebAssembly.Common';
 import { shouldAutoStart } from './BootCommon';
 import { Blazor } from './GlobalExports';
 import { WebStartOptions } from './Platform/WebStartOptions';
 import { attachStreamingRenderingListener } from './Rendering/StreamingRendering';
 import { NavigationEnhancementCallbacks, attachProgressivelyEnhancedNavigationListener } from './Services/NavigationEnhancement';
-import { WebAssemblyComponentDescriptor } from './Services/ComponentDescriptorDiscovery';
-import { ServerComponentDescriptor } from './Services/ComponentDescriptorDiscovery';
-import { RootComponentManager } from './Services/RootComponentManager';
-import { WebRendererId } from './Rendering/WebRendererId';
+import { ComponentDescriptor } from './Services/ComponentDescriptorDiscovery';
+import { RootComponentManager, attachAutoModeResolver } from './Services/RootComponentManager';
 import { DescriptorHandler, attachComponentDescriptorHandler, registerAllComponentDescriptors } from './Rendering/DomMerging/DomSync';
 
 let started = false;
 let isPerformingEnhancedNavigation = false;
 let webStartOptions: Partial<WebStartOptions> | undefined;
+let hasWebAssemblyLoaded = false;
 
-const circuitRootComponents = new RootComponentManager(WebRendererId.Server);
-const webAssemblyRootComponents = new RootComponentManager(WebRendererId.WebAssembly);
+const rootComponentManager = new RootComponentManager();
 
 function boot(options?: Partial<WebStartOptions>) : Promise<void> {
   if (started) {
@@ -48,6 +46,15 @@ function boot(options?: Partial<WebStartOptions>) : Promise<void> {
 
   attachComponentDescriptorHandler(descriptorHandler);
   attachStreamingRenderingListener(options?.ssr, navigationEnhancementCallbacks);
+  attachAutoModeResolver(() => {
+    if (hasWebAssemblyLoaded) {
+      startWebAssemblyIfNotStarted();
+      return 'webassembly';
+    } else {
+      startCircuitIfNotStarted();
+      return 'server';
+    }
+  });
 
   if (!options?.ssr?.disableDomPreservation) {
     attachProgressivelyEnhancedNavigationListener(navigationEnhancementCallbacks);
@@ -58,16 +65,15 @@ function boot(options?: Partial<WebStartOptions>) : Promise<void> {
   return Promise.resolve();
 }
 
-function registerComponentDescriptor(descriptor: ServerComponentDescriptor | WebAssemblyComponentDescriptor) {
-  switch (descriptor.type) {
-    case 'server':
-      startCircuitIfNotStarted();
-      circuitRootComponents.registerComponentDescriptor(descriptor);
-      break;
-    case 'webassembly':
-      startWebAssemblyIfNotStarted();
-      webAssemblyRootComponents.registerComponentDescriptor(descriptor);
-      break;
+function registerComponentDescriptor(descriptor: ComponentDescriptor) {
+  rootComponentManager.registerComponentDescriptor(descriptor);
+
+  if (descriptor.type === 'auto') {
+    startLoadingWebAssembly();
+  } else if (descriptor.type === 'server') {
+    startCircuitIfNotStarted();
+  } else if (descriptor.type === 'webassembly') {
+    startWebAssemblyIfNotStarted();
   }
 }
 
@@ -86,29 +92,40 @@ function afterEnhancedNavigation() {
 
 function handleUpdatedDescriptors() {
   const shouldAddNewRootComponents = !isPerformingEnhancedNavigation;
-  circuitRootComponents.handleUpdatedRootComponents(shouldAddNewRootComponents);
-  webAssemblyRootComponents.handleUpdatedRootComponents(shouldAddNewRootComponents);
+  rootComponentManager.handleUpdatedRootComponents(shouldAddNewRootComponents);
 }
 
-let circuitStarted = false;
+let hasCircuitStarted = false;
 async function startCircuitIfNotStarted() {
-  if (circuitStarted) {
+  if (hasCircuitStarted) {
     return;
   }
 
-  circuitStarted = true;
-  await startCircuit(webStartOptions?.circuit, circuitRootComponents);
+  hasCircuitStarted = true;
+  await startCircuit(webStartOptions?.circuit, rootComponentManager);
   handleUpdatedDescriptors();
 }
 
-let webAssemblyStarted = false;
-async function startWebAssemblyIfNotStarted() {
-  if (webAssemblyStarted) {
+let hasStartedLoadingWebAssembly = false;
+async function startLoadingWebAssembly() {
+  if (hasStartedLoadingWebAssembly) {
     return;
   }
 
-  webAssemblyStarted = true;
-  await startWebAssembly(webStartOptions?.webAssembly, webAssemblyRootComponents);
+  hasStartedLoadingWebAssembly = true;
+  await loadWebAssemblyPlatform(webStartOptions?.webAssembly);
+  hasWebAssemblyLoaded = true;
+}
+
+let isStartingWebAssembly = false;
+async function startWebAssemblyIfNotStarted() {
+  if (isStartingWebAssembly) {
+    return;
+  }
+
+  isStartingWebAssembly = true;
+  await startWebAssembly(webStartOptions?.webAssembly, rootComponentManager);
+  hasWebAssemblyLoaded = true;
   handleUpdatedDescriptors();
 }
 
